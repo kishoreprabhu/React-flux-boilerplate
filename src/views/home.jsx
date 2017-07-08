8aa4bed5acf0eab454ac04de60a94bd23fc60382
kishoreprabhu <kishoreprabhu97@gmail.com>
2017-07-08 14:26:53 +0530

    =dynamic card static ui component completion

--- src/views/home.jsx
+++ src/views/home.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import SearchField from './component/searchfield/searchfield.jsx';
 import AppDescription from './appdescription.jsx';
+import PhotoTile from './component/dynamicphototile/dynamicphototile.jsx';
 
 export default class Home extends React.Component {
 	constructor(props) {
@@ -18,6 +19,7 @@ export default class Home extends React.Component {
 						 <SearchField fieldplaceholder="Search free high-resolution photos"/>
 						 <AppDescription/>
 						 <div className="content-area">
+							  <PhotoTile/>
 						 </div>
 					 </div>	
 				  );
